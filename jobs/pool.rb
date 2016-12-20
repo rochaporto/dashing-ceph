42236350152cccfe86e1ec953042e26c9c6dee23
Stephen McElroy <magusnebula@gmail.com>
2016-12-19 21:10:45 -0600

    Update pool.rb
    #Percentage was not showing the right ratio, changed vaule by replacing pretty with .to_s('TB')
    +    #this really should be done with the Max value as well to make sure no matter the size
    +    # Both values are in the same size value

--- jobs/pool.rb
+++ jobs/pool.rb
@@ -17,15 +17,21 @@ SCHEDULER.every '5s' do
   result = %x( timeout 3 ceph df -f json )
 
   # update total storage widget
+  # *updated this to work with Jewel release
   storage = JSON.parse(result)
-  used = storage['stats']['total_used'].to_i
-  total = storage['stats']['total_space'].to_i
+  used = storage['stats']['total_used_bytes'].to_i
+  total = storage['stats']['total_bytes'].to_i
+  
+  #Percentage was not showing the right ratio, changed vaule by replacing pretty with .to_s('TB')
+    #this really should be done with the Max value as well to make sure no matter the size
+    # Both values are in the same size value
   send_event('storage',
     {
-      value: Filesize.from("#{used} KB").pretty.split(' ').first.to_f,
+      value: Filesize.from("#{used} B").to_s('TB'),
       min: 0,
-      max: Filesize.from("#{total} KB").pretty.split(' ').first.to_f,
-      moreinfo: Filesize.from("#{used} KB").pretty + " out of " + Filesize.from("#{total} KB").pretty
+      max: Filesize.from("#{total} B").pretty.split(' ').first.to_f,
+      #max:storage['stats']['total_bytes'].pretty.to_i,
+      moreinfo: Filesize.from("#{used} B").pretty + " out of " + Filesize.from("#{total} B").pretty
     }
   )
 
